7826b9923440ee9d5f0832a11a746e7575bc353e
Craig Roberts <craig.roberts11@outlook.com>
2023-05-24 13:28:44 +0930

    Update App.js
    removed duplicate entries, merge annotations and references to google books project

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SavedBooks from "./pages/SavedBooks";
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import Home from "./pages/Home";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import CreateGroupForm from "./pages/CreateGroupForm";
 import Group from "./pages/Group";
-=======
 import Header from "./components/Header";
 import Footer from "./components/Footer";
->>>>>>> main
-=======
-import CreateGroupForm from "./pages/CreateGroupForm";
-import Group from "./pages/Group";
->>>>>>> 519fb029f4193fdc36ca43c3222ebfa8aa643bcd
 
 // construct the GraphQL endpoint
 const httpLink = createHttpLink({
@@ -47,7 +38,6 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/saved" element={<SavedBooks />} />
             <Route path="/create-group" element={<CreateGroupForm />} />
             <Route path="/group" element={<Group />} />
             <Route path="*" element={<h1 className="display-2">Wrong page!</h1>} />
