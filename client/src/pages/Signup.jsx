d175b8d7d84d1334d8325a4748461a2b26556d53
Craig Roberts <craig.roberts11@outlook.com>
2023-05-25 15:47:13 +0930

    added modal for file upload

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
 const Signup = () => {
   const [formState, setFormState] = useState({
-    username: '',
-    email: '',
-    password: '',
+    username: "",
+    email: "",
+    password: "",
   });
   const [addUser, { error, data }] = useMutation(ADD_USER);
 
@@ -46,8 +46,7 @@ const Signup = () => {
           <div className="card-body">
             {data ? (
               <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
+                Success! You may now head <Link to="/">back to the homepage.</Link>
               </p>
             ) : (
               <form onSubmit={handleFormSubmit}>
@@ -77,28 +76,25 @@ const Signup = () => {
                 />
                 <input
                   className="form-input"
-                  placeholder="******"
+                  placeholder="Password"
                   name="password"
                   type="password"
                   value={formState.password}
                   onChange={handleChange}
                 />
-                <button
-                  className="btn"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
+                <button className="btn" style={{ cursor: "pointer" }} type="submit">
                   Submit
                 </button>
-                <p>Already have an account?<Link className="m-2" to="/login">Login Here!</Link></p>
+                <p>
+                  Already have an account?
+                  <Link className="m-2" to="/login">
+                    Login Here!
+                  </Link>
+                </p>
               </form>
             )}
 
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
+            {error && <div className="my-3 p-3 bg-danger text-white">{error.message}</div>}
           </div>
         </div>
       </div>
--- client/src/pages/UserGroup.jsx
+++ client/src/pages/UserGroup.jsx
@@ -1,18 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { DELETE_SINGLE_PHOTO, DELETE_MANY_PHOTOS } from "../utils/mutations";
+import Modal from "react-bootstrap/Modal";
+import { DELETE_SINGLE_PHOTO, DELETE_MANY_PHOTOS, SAVE_PHOTO } from "../utils/mutations";
 import { GET_PHOTOS_FOR_GROUP, GET_FILE_UPLOAD_URL } from "../utils/queries";
 import { useMutation, useQuery, useLazyQuery } from "@apollo/client";
 import { usePhotochuteContext } from "../utils/globalState";
+import uploadFileToBlob from "../utils/blobStorage";
+import { useLocation } from "react-router-dom";
 import PhotoGrid from "../components/PhotoGrid";
 import "../assets/css/UserGroup.css";
 
-const Group = ({ name = "The Walruses", groupOwner, members }) => {
+const Group = () => {
+  const location = useLocation();
+  // const { name, groupOwner, members } = location.state;
+  const name = "The Walruses";
+  const [showModal, setShowModal] = useState(false);
+  const [fileSelected, setFileSelected] = useState();
+  const [uploading, setUploading] = useState(false);
+  const [getFileUploadUrl] = useLazyQuery(GET_FILE_UPLOAD_URL);
+  const [fileTypeValidationError, setFileTypeValidationError] = useState(false);
+  const [savePhoto] = useMutation(SAVE_PHOTO);
+
+  /* THIS IS UNTIL THE USER PROFILE PAGE IS COMPLETE */
+  const groupId = "646ec77bcc84812aacaccd3e";
+  const serialisedGroupName = "the-walruses-a4e50880-faa3-11ed-a777-394dc37315ec";
+  const groupOwner = "";
+  const members = [];
+
   const {
     loading: loadingPhotos,
     error: errorLoadingPhotos,
     data: photos,
   } = useQuery(GET_PHOTOS_FOR_GROUP, {
-    variables: { groupName: name },
+    variables: { groupName: serialisedGroupName },
   });
 
   const [selectedPhotos, setSelectedPhotos] = useState([]);
@@ -46,16 +65,76 @@ const Group = ({ name = "The Walruses", groupOwner, members }) => {
     console.log(response);
   };
 
+  const onFileChange = (event) => {
+    // capture file into state
+    setFileSelected(event.target.files[0]);
+
+    // validateFileType(fileSelected.type);
+  };
+
+  const onFileUpload = async () => {
+    if (fileSelected && fileSelected?.name) {
+      // prepare UI
+      setUploading(true);
+      try {
+        // TODO - find a way to get the current group's containerName instead of hard coded value
+        const groupName = "the-walruses-a4e50880-faa3-11ed-a777-394dc37315ec";
+        const urlData = await getFileUploadUrl({
+          variables: {
+            groupName,
+            blobName: fileSelected.name,
+          },
+        });
+
+        const { fileUrl } = urlData.data.getFileUploadUrl;
+
+        // *** UPLOAD TO AZURE STORAGE ***
+        const fileUploadUrl = await uploadFileToBlob(fileSelected, fileUrl);
+        console.log(fileUrl);
+
+        // reset state/form
+      } catch (error) {
+        console.log(JSON.stringify(error, null, 2));
+      }
+      setFileSelected(null);
+      setUploading(false);
+    }
+  };
+
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
+
   return (
     <div>
       <h1 className="text-center">{name}</h1>
       <div className="group-container">
         <div className="members-container">
+          <h3>Owner</h3>
+          <p>Craig</p>
           <h3>Members</h3>
           <p>Shae</p>
           <p>Lucien</p>
-          <p>Craig</p>
+          <button className="btn" onClick={openModal}>
+            Add Photos
+          </button>
         </div>
+        <Modal show={showModal} onHide={closeModal}>
+          <Modal.Header closeButton>Choose a photo to upload</Modal.Header>
+          <Modal.Body>
+            <div>
+              <input type="file" onChange={onFileChange} />
+              <button
+                type="submit"
+                onClick={onFileUpload}
+                // disabled={fileTypeValidationError}
+              >
+                Upload!
+              </button>
+            </div>
+            {uploading && <div>Uploading</div>}
+            {fileTypeValidationError && <div>File must be of type jpg, jpeg or png</div>}
+          </Modal.Body>
+        </Modal>
         <PhotoGrid
           thumbnails={new Array(30).fill({
             fileName: "test-thumbnail.jpg",
